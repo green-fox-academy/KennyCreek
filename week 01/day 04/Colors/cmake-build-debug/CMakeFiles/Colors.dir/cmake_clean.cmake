e46772fb77c3502d6c9d73e20d100cac4b14b83c
KennyCreek <vezer.kende@gmail.com>
2019-04-04 17:19:54 +0200

    Colors

---
+++ week 01/day 04/Colors/cmake-build-debug/CMakeFiles/Colors.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Colors.dir/main.cpp.obj"
+  "Colors.pdb"
+  "Colors.exe"
+  "Colors.exe.manifest"
+  "libColors.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Colors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
