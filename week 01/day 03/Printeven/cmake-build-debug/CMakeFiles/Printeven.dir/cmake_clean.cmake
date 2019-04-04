17c7cbb32fcadae57abd83c51050c8ef59a94d10
KennyCreek <vezer.kende@gmail.com>
2019-04-04 19:47:46 +0200

    PrintEven

---
+++ week 01/day 03/Printeven/cmake-build-debug/CMakeFiles/Printeven.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Printeven.dir/main.cpp.obj"
+  "Printeven.pdb"
+  "Printeven.exe"
+  "Printeven.exe.manifest"
+  "libPrinteven.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Printeven.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week 01/day 03/Printeven/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // Create a program that prints all the even numbers between 0 and 500
+    int i;
+  for ( int i = 0; i< 500; i++){
+      if (i%2 == 0){
+          std::cout<< i<< std::endl;
+      }
+
+  }
+
+
+
+
+
+
+    return 0;
+}
