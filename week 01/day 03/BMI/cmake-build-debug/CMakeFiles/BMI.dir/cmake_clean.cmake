c1b5aeadf27d586655972eb556ef20ccd9650580
KennyCreek <vezer.kende@gmail.com>
2019-04-04 19:14:13 +0200

    BMI

---
+++ week 01/day 03/BMI/cmake-build-debug/CMakeFiles/BMI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BMI.dir/main.cpp.obj"
+  "BMI.pdb"
+  "BMI.exe"
+  "BMI.exe.manifest"
+  "libBMI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BMI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
