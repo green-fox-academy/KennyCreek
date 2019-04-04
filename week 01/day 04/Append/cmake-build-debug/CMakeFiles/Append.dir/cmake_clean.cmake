edecf3d33e284c2ecc23e92293f585ca4aacb9bd
KennyCreek <vezer.kende@gmail.com>
2019-04-04 15:17:23 +0200

    Append

---
+++ week 01/day 04/Append/cmake-build-debug/CMakeFiles/Append.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Append.dir/main.cpp.obj"
+  "Append.pdb"
+  "Append.exe"
+  "Append.exe.manifest"
+  "libAppend.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Append.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
