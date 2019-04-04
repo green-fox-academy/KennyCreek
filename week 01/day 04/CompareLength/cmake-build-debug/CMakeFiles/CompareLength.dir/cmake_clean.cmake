42b51fa8730334d7c91c212c4e5e30c5b2d6ea87
KennyCreek <vezer.kende@gmail.com>
2019-04-04 15:18:27 +0200

    CL

---
+++ week 01/day 04/CompareLength/cmake-build-debug/CMakeFiles/CompareLength.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompareLength.dir/main.cpp.obj"
+  "CompareLength.pdb"
+  "CompareLength.exe"
+  "CompareLength.exe.manifest"
+  "libCompareLength.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompareLength.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
