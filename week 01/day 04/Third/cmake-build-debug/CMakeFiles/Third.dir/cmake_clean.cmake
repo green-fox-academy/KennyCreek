6486bf80eb9850d16b337e0c8388badbbe99c16b
KennyCreek <vezer.kende@gmail.com>
2019-04-04 15:21:41 +0200

    Third

---
+++ week 01/day 04/Third/cmake-build-debug/CMakeFiles/Third.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Third.dir/main.cpp.obj"
+  "Third.pdb"
+  "Third.exe"
+  "Third.exe.manifest"
+  "libThird.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Third.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
