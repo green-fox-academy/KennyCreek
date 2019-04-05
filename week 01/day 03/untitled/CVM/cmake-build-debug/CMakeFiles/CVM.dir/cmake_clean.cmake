dfbeb1f5b4d7db37ae3f0226720d1d1edb6f27eb
KennyCreek <vezer.kende@gmail.com>
2019-04-05 15:02:01 +0200

    CVM

---
+++ week 01/day 03/untitled/CVM/cmake-build-debug/CMakeFiles/CVM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVM.dir/main.cpp.obj"
+  "CVM.pdb"
+  "CVM.exe"
+  "CVM.exe.manifest"
+  "libCVM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week 01/day 03/untitled/CVM/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+
+    double a = 24;
+    int out = 0;
+
+    int aasint = a;
+
+    if (aasint % 2 == 0) {
+        out = out + 1;
+
+
+        // if a is even increment out by one
+
+        std::cout << out << std::endl;
+    }
+
+
+    int b = 13;
+    std::string out2 = "";
+    // if b is between 10 and 20 set out2 to "Sweet!"
+    // if less than 10 set out2 to "Less!",
+    // if more than 20 set out2 to "More!"
+
+    if (b < 20 && b > 10) {
+        out2 = "Sweet";
+    } else if (b < 10) {
+        out2 = "Less";
+    } else if (b > 20) {
+        out2 = "More";
+    }
+
+
+    std::cout << out2 << std::endl;
+
+
+    int c = 50;
+    int credits = 100;
+    bool isBonus = false;
+    // if credits are at least 50,
+    // and isBonus is false decrement c by 2
+    // if credits are smaller than 50,
+    // and isBonus is false decrement c by 1
+    // if isBonus is true c should remain the same
+
+    int decr = 2;
+
+    if (isBonus == false){
+        if 
+    }
+
+
+
+
+
+
+     << c << std::endl;
+
+
+    int d = 8;
+    int time = 120;
+    std::string out3 = "";
+    // if d is dividable by 4
+    // and time is not more than 200
+    // set out3 to "check"
+    // if time is more than 200
+    // set out3 to "Time out"
+    // otherwise set out3 to "Run Forest Run!"
+
+    std::cout << out3 << std::endl;
+
+    return 0;
+}
