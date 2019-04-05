44b25d349027a152eff404e4e7b927e8e8f3f9c7
KennyCreek <vezer.kende@gmail.com>
2019-04-05 12:28:23 +0200

    PrintBigger

---
+++ week 01/day 03/PrintBigger/cmake-build-debug/CMakeFiles/PrintBigger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrintBigger.dir/main.cpp.obj"
+  "PrintBigger.pdb"
+  "PrintBigger.exe"
+  "PrintBigger.exe.manifest"
+  "libPrintBigger.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrintBigger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week 01/day 03/PrintBigger/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    std::cout<<"Enter your numbers"<<std::endl;
+    int number1;
+    int number2;
+
+    std::cin>>number1;
+    std::cin>>number2;
+
+    if (number1>number2){
+        std::cout<<number1<<" is bigger"<<std::endl;}
+        else if(number1<number2){
+            std::cout<<number1<<" is smaller"<<std::endl;
+        }
+
+
+
+
+
+    return 0;
+}
